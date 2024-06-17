879c99968e5ac564c3b75774d84d9e542a9026f6
James Mills <james.mills@aie.edu.au>
2024-06-18 09:33:57 +1000

    Initial editor setup

---
+++ premake/lua/data/editor.lua
@@ -0,0 +1,75 @@
+require("utilities.string-utils")
+ids = require("utilities.ids")
+
+local editor=
+{
+    name = "",
+    additional_files={}
+}
+
+function editor:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function editor.parse(lines)
+    local new_editor = editor:new{name = "",additional_files={}}
+
+    new_editor:get_name(lines)
+    table.remove(lines, 1)
+    new_editor:get_additional_files(lines)
+
+    return new_editor
+end
+
+function editor:get_name(lines)
+    line = remove_tab(lines[1])
+
+    if contains(line, ids.name) then
+        self.name = value_from_len(line, ids.name, 3, 2)
+    end
+end
+
+function editor:get_additional_files(lines)
+    line = remove_tab(lines[1])
+
+    if contains(line, ids.additional_files) then
+        line = value_from_len(line, ids.additional_files, 3, 2)
+
+        line = split(line, ',')
+
+        for l, v in pairs(line) do
+            self.additional_files[l - 1] = no_quotes(v)
+        end
+    end
+end
+
+function editor.get_remaining_data(file)
+    local data = {}
+
+    while true do
+        local line = file:read()
+        if contains(line, "modules") then 
+            break
+        end
+
+        if not contains(line, "editor") or line[1] ~= '[' or line[1] ~= ']' or line[1] ~= '{' or line[1] ~= '}' then
+            data[#data+1] = line
+        end
+    end
+
+    return data
+end
+
+function editor.read(file)
+    local lines = editor.get_remaining_data(file)
+
+    table.remove(lines, 1)
+    table.remove(lines, 1)
+
+    return editor.parse(lines)
+end
+
+return editor
--- premake5.lua
+++ premake5.lua
@@ -6,10 +6,12 @@ local engine_props = properties.read(current_dir .. "\\engine.properties")
 local outputdir = "%{cfg.buildcfg}/%{cfg.system}-%{cfg.architecture}"
 
 tb = require("generators.testbed")
+ed = require("generators.editor")
 ws = require("generators.workspace")
 pj = require("generators.projects")
 
 ws.generate(engine_props)
+ed.generate(engine_props, outputdir)
 tb.generate(engine_props, outputdir)
 
 group (engine_props.name)
